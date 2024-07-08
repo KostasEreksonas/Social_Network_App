5860abd4d815622ddd596a3e3299ede2f9e89b60
KostasEreksonas <k.ereksonas@gmail.com>
2024-07-08 23:24:23 +0300

    Created a base Post class

--- public/SocNet/classes/Post.php
+++ public/SocNet/classes/Post.php
@@ -1,4 +1,41 @@
 <?php
+require_once 'Database.php';
 class Post {
+    protected $db;
 
+    public function __construct ()
+    {
+        $this->db = new Database();
+    }
+
+    public function createPost() : void
+    {
+        /*
+         * Creates a post and record it in a database
+         */
+    }
+
+    public function getPosts() : string
+    {
+        /*
+         * Returns all posts for an user
+         */
+        return 'posts';
+    }
+
+    public function getPost() : string
+    {
+        /*
+         * Return a specific post from an user based on condition(s)
+         */
+        return 'post';
+    }
+
+    public function countPosts() : int
+    {
+        /*
+         * Return count of user posts
+         */
+        return 1;
+    }
 }
